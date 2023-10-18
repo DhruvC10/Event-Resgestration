488cdfd30fcc2733812d3d947384e221e9f9b70b
Suhaan-Bhandary <suhaanbhandary1@gmail.com>
2023-10-18 13:00:25 +0530

    feat: Added cors and prevent sleep configs

--- registeration-portal-server/src/app.ts
+++ registeration-portal-server/src/app.ts
@@ -1,10 +1,22 @@
 import express from 'express';
 import routes from './routes';
+import cors from 'cors';
 
 const app = express();
 
 // Routes
 app.use('/ticket', routes.ticketRoutes);
+
+// Middle ware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env['CLIENT_BASE_URL'],
+    methods: ['GET', 'POST'],
+    credentials: true,
+  }),
+);
 
 app.get('/', (req, res) => {
   res.send('hello from ts-app');
--- registeration-portal-server/src/server.ts
+++ registeration-portal-server/src/server.ts
@@ -1,11 +1,17 @@
-import app from './app';
+import { config } from 'dotenv';
 import { connectDB } from './config/database';
 
 // DB connect
+config();
 connectDB();
 
+import app from './app';
+import preventServerSleep from './utils/preventServerSleep';
 const PORT = process.env['PORT'] || 8080;
 
 app.listen(PORT, () => {
   console.log(`Listening at PORT: ${PORT}`);
 });
+
+// Create a Job once App is started
+preventServerSleep();
